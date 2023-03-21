67f73b6832c4ff8678509c43b64b34291350deed
ALEJANDRO PRIETO GARCÍA <aprieto@vienext.com>
2023-03-22 00:46:07 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+const dotenv = require('dotenv')
+dotenv.config()
+const bodyParser = require('body-parser')
+const { response } = require('express')
+const app = express()
+const port = 3006
+const baseUrl = 'https://api.sendinblue.com/v3/smtp/email'
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+var jsonParser = bodyParser.json()
+app.post('/email', jsonParser, cors(corsOptions), async (req, res) => {
+    console.log(req)
+    const options = {
+        method: 'POST',
+        url: baseUrl,
+        withCredentials: false,
+        data: {
+            sender: {
+                name: req.body.sender.name.toString(),
+                email: req.body.sender.email.toString(),
+            },
+            to: [
+                {
+                    email: req.body.to[0].email.toString(),
+                    name: req.body.to[0].name.toString()
+                }
+            ],
+            subject: req.body.subject.toString(),
+            htmlContent: req.body.htmlContent.toString()
+        },
+        headers: {
+            accept: "application/json",
+            "api-key": `${process.env.API_KEY_PORTFOLIO}`,
+            "content-type": "application/json"
+        }
+    }
+    const resp = await axios.request(options)
+    res.send(resp.data)
+})
+
+app.use(cors())
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
